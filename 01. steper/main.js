01524e383ab446c833c0586a95035d98e5a5a492
SpearJin <ckdwls2525@naver.com>
2022-01-04 10:31:09 +0900

    steper_js: 숫자 감소&증가 기능구현

---
+++ 01. steper/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const $steper = document.querySelector('.steper');
+const $steperCount = document.querySelector('.steper_count');
+let countValue = Number($steperCount.textContent);
+// steperCount의 value값은 string이므로 number타입으로 변환함
+
+$steper.addEventListener('click', (e) => {
+  const target = e.target;
+  const selected = target.className.split(' ');
+  // target에 class이름이 여러개이면 배열로 재정의함
+  if (selected.includes('btn-plus')) {
+    countValue++;
+  } else if (selected.includes('btn-minues')) {
+    countValue = countValue > 0 ? countValue - 1 : 0;
+    // 0이하로 떨어지지 않게 처리함
+  }
+  $steperCount.textContent = countValue;
+});
